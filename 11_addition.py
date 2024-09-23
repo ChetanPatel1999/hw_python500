34ef4338e19cd4b444a1db858b91c6e3ea4ad5f5
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2024-09-23 15:23:06 +0530

    21/09

---
+++ 11_addition.py
@@ -0,0 +1,7 @@
+#wap to take two number from user and display their sum.
+num1=int(input("enter frist number :" ))
+num2=int(input("enter second number :" ))
+print(f"value of num1 ={num1}")
+print(f"value of num2 ={num2}")
+add=num1+num2
+print(f"sum of {num1} and {num2} = {add}")
---
+++ 13_string.py
@@ -0,0 +1,7 @@
+#wap to take two string form user and combine them.
+s1=input("enter frist string :")
+s2=input("enter second string :")
+print("value of s1 = ",s1)
+print("value of s2 = ",s2)
+city=s1+" "+s2
+print(f"cities : {city}")
