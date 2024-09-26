e3093bd2fbef22407d4f16135bf18542644069f8
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2024-09-26 15:19:39 +0530

    25/09

---
+++ 19_numberswap.py
@@ -0,0 +1,11 @@
+#wap to swap two values using third variable.
+a=int(input("enter a value : "))
+b=int(input("enter b value : "))
+print("before swaping values.... ")
+print(f"value of a = {a}\nvalue of b = {b}")
+c=a
+a=b
+b=c
+print("after swaping values.... ")
+print(f"value of a = {a}\nvalue of b = {b}")
+
---
+++ 20_numberswap.py
@@ -0,0 +1,11 @@
+#wap to swap two values without using third variable.
+a=int(input("enter a value : "))#12
+b=int(input("enter b value : "))#5
+print("before swaping values.... ")
+print(f"value of a = {a}\nvalue of b = {b}")
+a=a+b #17
+b=a-b #12
+a=a-b #5
+print("after swaping values.... ")
+print(f"value of a = {a}\nvalue of b = {b}")
+
