c6a3bcf4e6afbbf5e2495baf6c716fb5568f026c
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-03-29 15:15:28 +0530

    3/29

--- 118_dict.py
+++ 118_dict.py
@@ -20,9 +20,14 @@ number={1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',
     100:"one hundred",
     200:"two hundred ",
     300:"three hundred",
-    400: "four hundred"
+    400: "four hundred",
+    500: "five hundred",
+    600: "six hundred",
+    700: "seven hundred",
+    800: "eight hundred",
+    900: "nine hundred",
 }
-num=int(input("enter a num : "))#234
+num=int(input("enter a num : "))#511
 p=len(str(num))
 if p==2:
     l=num%10
@@ -30,7 +35,7 @@ if p==2:
     print(number[num]) if num in number else print(f"{number[m]} {number[l]}")
 else :
     c=(num//100)*100
-    num=num%100
+    m = ((num//10)%10)*10 if (num%100) not in number else (num%100)
+    print(m)
     l=num%10
-    m=num-l
-    print(number[num]) if num in number else print(f"{number[c]}  {number[m]} {number[l]}")  
+    print(number[num]) if num in number else print(f"{number[c]} ", number[m] if m in number and ('0' not in str(m)) else f"{number[m]} {number[l]}" )  
